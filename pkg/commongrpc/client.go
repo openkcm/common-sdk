d1b24636dfc2bdb9753ca71cd97e5835ec8f2035
Nicolae Nicora <n.nicora@sap.com>
2025-07-22 14:30:16 +0200

    feat: include the common grpc methods

---
+++ pkg/commongrpc/client.go
@@ -0,0 +1,65 @@
+package commongrpc
+
+import (
+	"errors"
+
+	"github.com/openkcm/common-sdk/pkg/commoncfg"
+	"github.com/openkcm/common-sdk/pkg/grpcpool"
+	"github.com/openkcm/common-sdk/pkg/otlp"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+)
+
+var (
+	EmptyAddressErr = errors.New("grpc address is empty")
+)
+
+type PooledClient interface {
+	SetPool(*grpcpool.Pool)
+}
+
+func NewPooledClient(client PooledClient, cfg *commoncfg.GRPCClient, dialOptions ...grpc.DialOption) error {
+	if cfg.Address == "" {
+		return EmptyAddressErr
+	}
+	opts := make([]grpc.DialOption, 0)
+	opts = append(opts,
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:    cfg.Attributes.KeepaliveTime,
+			Timeout: cfg.Attributes.KeepaliveTimeout,
+		}),
+		grpc.WithStatsHandler(otlp.NewClientHandler()),
+	)
+	opts = append(opts, dialOptions...)
+
+	clientPool, err := grpcpool.New(createFactory(cfg.Address, opts...))
+	if err != nil {
+		return err
+	}
+
+	client.SetPool(clientPool)
+	return nil
+}
+
+func createFactory(address string, dialOptions ...grpc.DialOption) grpcpool.ClientFactory {
+	return func() (*grpc.ClientConn, error) {
+		return grpc.NewClient(address, dialOptions...)
+	}
+}
+
+func NewClient(cfg *commoncfg.GRPCClient, dialOptions ...grpc.DialOption) (*grpc.ClientConn, error) {
+	if cfg.Address == "" {
+		return nil, EmptyAddressErr
+	}
+	opts := make([]grpc.DialOption, 0)
+	opts = append(opts,
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:    cfg.Attributes.KeepaliveTime,
+			Timeout: cfg.Attributes.KeepaliveTimeout,
+		}),
+		grpc.WithStatsHandler(otlp.NewClientHandler()),
+	)
+	opts = append(opts, dialOptions...)
+
+	return grpc.NewClient(cfg.Address, opts...)
+}
---
+++ pkg/commongrpc/server.go
@@ -0,0 +1,46 @@
+package commongrpc
+
+import (
+	"github.com/openkcm/common-sdk/pkg/commoncfg"
+	"github.com/openkcm/common-sdk/pkg/health"
+	"github.com/openkcm/common-sdk/pkg/otlp"
+	"google.golang.org/grpc"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
+)
+
+// NewServer create the grpc server
+func NewServer(cfg *commoncfg.GRPCServer, opts ...grpc.ServerOption) *grpc.Server {
+	var options []grpc.ServerOption
+
+	options = append(options,
+		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+			MinTime:             cfg.EfPolMinTime,             // If a client pings more than once every 15 sec, terminate the connection
+			PermitWithoutStream: cfg.EfPolPermitWithoutStream, // Allow pings even when there are no active streams
+		}),
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionIdle:     cfg.Attributes.MaxConnectionIdle,
+			MaxConnectionAge:      cfg.Attributes.MaxConnectionAge,
+			MaxConnectionAgeGrace: cfg.Attributes.MaxConnectionAgeGrace,
+			Time:                  cfg.Attributes.Time,
+			Timeout:               cfg.Attributes.Timeout,
+		}),
+		grpc.MaxRecvMsgSize(cfg.MaxRecvMsgSize),
+		grpc.StatsHandler(otlp.NewServerHandler()),
+	)
+
+	opts = append(opts, opts...)
+
+	grpcServer := grpc.NewServer(opts...)
+
+	if cfg.Flags.Reflection {
+		reflection.Register(grpcServer)
+	}
+
+	if cfg.Flags.Health {
+		healthpb.RegisterHealthServer(grpcServer, &health.GRPCServer{})
+	}
+
+	return grpcServer
+}
